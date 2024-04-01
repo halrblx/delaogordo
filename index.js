a91c205b36043f512ed32d7fd5bbc145607b35ff
halrblx <165708175+halrblx@users.noreply.github.com>
2024-04-01 20:39:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Viendo Málaga:RP💙","halrraiser💜"];
 
 
 let currentIndex = 0;
